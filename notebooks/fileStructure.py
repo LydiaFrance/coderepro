68082c7d5fc6162ec728046bec733d6c4cbd042b
Lydia France <lfrance@turing.ac.uk>
2024-06-27 14:37:32 +0100

    Ignore target repo directory in git

--- notebooks/fileStructure.py
+++ notebooks/fileStructure.py
@@ -1,4 +1,4 @@
-from github import Github
+# from github import Github
 from ollama import Client
 client = Client(host='http://localhost:11434')
 def talk_to_llama(context, prompt):
@@ -11,27 +11,29 @@ def talk_to_llama(context, prompt):
   ])
   return response["message"]["content"]
 
-gh = Github()
+# gh = Github()
 
 #for repo in gh.get_user().get_repos():
 #    print(repo.name)
-repo = gh.get_repo("LydiaFrance/coderepro")
+# repo = gh.get_repo("LydiaFrance/coderepro")
 
-contents = repo.get_contents("")
+# contents = repo.get_contents("")
 
-dirfiles = []
-while contents:
+# dirfiles = []
+# while contents:
 
-    file_content = contents.pop(0)
+#     file_content = contents.pop(0)
 
-    if file_content.type == "dir":
+#     if file_content.type == "dir":
 
-        contents.extend(repo.get_contents(file_content.path))
+#         contents.extend(repo.get_contents(file_content.path))
 
-    else:
+#     else:
 
-        #print(file_content)
-        dirfiles.append(file_content)
+#         #print(file_content)
+#         dirfiles.append(file_content)
+
+
 
 prompt = "I want to create a python package from my github repo. The file structure is given. I need detailed advice on what files are missing or to include to make this good software."
 print(talk_to_llama(dirfiles, prompt))

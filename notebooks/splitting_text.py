1c40780087391d4e9f7c429bd457eafbb1d69484
samvidav <samvida.venkatesh@gmail.com>
2024-06-27 16:28:36 +0100

    Change nchar to variable

--- notebooks/splitting_text.py
+++ notebooks/splitting_text.py
@@ -1,9 +1,14 @@
+NCHAR=7000
+
+def add_fname(file_path):
+    # bla bla samvida will do this
+
 def split_by_class(file_path, keyword):
     # Open the target file and read its content
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
     # Check if the content length is over 7000 characters
-    if len(content) > 7000:
+    if len(content) > NCHAR:
         # Split the content by the keyword
         parts = content.split(keyword)
     
@@ -27,7 +32,7 @@ split_text = split_by_class(file_path, keyword)
 
 def split_by_def(file_path, keyword):
     # Check if the content length is over 7000 characters
-    if len(split_text) > 7000:
+    if len(split_text) > NCHAR:
         # Split the content by the keyword
         parts = content.split(keyword)
     
@@ -43,6 +48,23 @@ def split_by_def(file_path, keyword):
     
     # Return the list of split text
     return further_split_text
+
+from langchain_text_splitters import CharacterTextSplitter
+
+text_splitter = CharacterTextSplitter(
+    separator="\n\n",
+    chunk_size=NCHAR,
+    chunk_overlap=0,
+    length_function=len,
+    is_separator_regex=False,
+)
+
+# Only read in N+1 characters as we only want 
+# to keep the first chunk anyway
+def split_by_character_number(input_text, output_fname):
+    text_to_chunk = input_text[:NCHAR+1]
+    chunked_text = text_splitter.create_documents([text_to_chunk])
+    return chunked_text[0].page_content
 
 # Example usage
 new_keyword = 'def '

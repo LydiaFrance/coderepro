3a33e1e10d5b60cb0491b5890018899c34fe72d0
Brian Moser <brian.moser@cern.ch>
2024-06-28 14:30:06 +0100

    change file structure

--- src/coderepro/make_summary_table.py
+++ src/coderepro/make_summary_table.py
@@ -7,7 +7,8 @@ import json
 
 test_data_path = "./temp_repo/output/"
 
-categories = ["General checks", "Code quality", "Documentation", "Testing", "Summary"]
+categories = ["Code Quality", "Documentation", "Testing", "Notebooks"]
+file_suffixes = ["codequality", "documentation", "testing", "notebooks"]
 
 #let's do category, text, score for now
 feedback_table = [["Category", "Feedback", "Score"]]
@@ -28,7 +29,7 @@ feedback_table.append(fc_table_entry)
 #and now to BERT
 sum_of_scores = 0
 for idx, cat in enumerate(categories): 
-    with open(os.path.join(test_data_path, f"response_{idx+1}.txt")) as fin:
+    with open(os.path.join(test_data_path, f"response_{file_suffixes[idx]}_1.txt")) as fin:
         raw_text = fin.read()
         eval_text = raw_text.replace('\n', '').replace("*", " ")
         sentiment_score = test_bert.evaluate_bert(eval_text)

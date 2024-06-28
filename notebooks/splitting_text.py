5b5a12d07fdc07c2a32e71331a701845c74eae36
Lydia France <lfrance@turing.ac.uk>
2024-06-28 14:16:29 +0100

    Add functions

--- notebooks/splitting_text.py
+++ notebooks/splitting_text.py
@@ -1,9 +1,21 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 import folder_to_text
 from folder_to_text import target_repo_path, converted_notebooks, python_files, md_files, test_files
 import os
 
 NCHAR=7000
+
+def clear_output_dir(output_path):
+    # Make the folder if it doesn't exist
+    # Clear the folder if it exists, then recreate it
+    if os.path.exists(output_path):
+        for filename in os.listdir(output_path):
+            file_path = os.path.join(output_path, filename)
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+        # No need to recreate the test_outputs directory as it's not deleted
+    else:
+        # Make the folder if it doesn't exist
+        os.makedirs(output_path)
 
 def add_fname_to_content(file_path):
     # Open the target file and read its content
@@ -119,22 +131,29 @@ def chunk_notebooks(nbcontent):
 
     return combined_splits
 
+def write_chunks_to_files(chunked_text, output_directory):
+    """
+    Writes each chunk of text to a separate file in the specified output directory.
+
+    Parameters:
+    - chunked_text: A list of text chunks to be written to files.
+    - output_directory: The directory where the chunk files will be saved.
+    """
+    for index, text in enumerate(chunked_text):
+        output_fname = os.path.join(output_directory, f"chunk{index}.txt")
+        with open(output_fname, "w", encoding='utf-8') as f:
+            f.write(text)
+
+
+
 # Create a new folder inside the temp_repo to store chunked text
 path_to_temp = os.path.dirname(target_repo_path)
 text_outputs = os.path.join(path_to_temp, "output/chunked_text_for_llms")
 
-# Make the folder if it doesn't exist
-if not os.path.exists(text_outputs):
-    os.makedirs(text_outputs)
-
-
-# For notebooks
+# ------- For notebooks -------
+# Make the folder if it doesn't exist, clear files if it does
 notebook_outputs = os.path.join(text_outputs, "notebooks")
-
-# Make the folder if it doesn't exist
-if not os.path.exists(notebook_outputs):
-    os.makedirs(notebook_outputs)
-
+clear_output_dir(notebook_outputs)
 
 content_to_chunk = ""
 for file in converted_notebooks:
@@ -142,18 +161,13 @@ for file in converted_notebooks:
 
 chunked_notebooks = chunk_notebooks(content_to_chunk)
 
-# Print the chunks
-for index, text in enumerate(chunked_notebooks):
-    output_fname = notebook_outputs + "/chunk" + str(index) + ".txt"
-    with open(output_fname, "w", encoding='utf-8') as f:
-        f.write(text)
+# Write the chunks to files
+write_chunks_to_files(chunked_notebooks, notebook_outputs)
 
-# For markdown documentation
+# ------- For markdown documentation -------
+# Make the folder if it doesn't exist, clear files if it does
 md_outputs = os.path.join(text_outputs, "markdown")
-
-# Make the folder if it doesn't exist
-if not os.path.exists(md_outputs):
-    os.makedirs(md_outputs)
+clear_output_dir(md_outputs)
 
 content_to_chunk = ""
 for file in md_files:
@@ -161,19 +175,13 @@ for file in md_files:
 
 chunked_md = chunk_markdown_scripts(content_to_chunk)
 
-# Print the chunks
-for index, text in enumerate(chunked_md):
-    output_fname = md_outputs + "/chunk" + str(index) + ".txt"
-    with open(output_fname, "w", encoding='utf-8') as f:
-        f.write(text)
+# Write the chunks to files
+write_chunks_to_files(chunked_md, md_outputs)
 
-# For python scripts
+# ------- For python scripts -------
+# Make the folder if it doesn't exist, clear files if it does
 py_outputs = os.path.join(text_outputs, "python_scripts")
-#os.mkdir(py_outputs)
-
-# Make the folder if it doesn't exist
-if not os.path.exists(py_outputs):
-    os.makedirs(py_outputs)
+clear_output_dir(py_outputs)
 
 content_to_chunk = ""
 for file in python_files:
@@ -181,15 +189,14 @@ for file in python_files:
 
 chunked_py = chunk_python_scripts(content_to_chunk)
 
-# Print the chunks
-for index, text in enumerate(chunked_py):
-    output_fname = py_outputs + "/chunk" + str(index) + ".txt"
-    with open(output_fname, "w", encoding='utf-8') as f:
-        f.write(text)
+# Write the chunks to files
+write_chunks_to_files(chunked_py, py_outputs)
 
 
-# # For test files (python scripts)
+# ------- For test files (tests) -------
+# Make the folder if it doesn't exist, clear files if it does
 test_outputs = os.path.join(text_outputs, "test_files")
+clear_output_dir(test_outputs)
 
 # Make the folder if it doesn't exist
 if not os.path.exists(test_outputs):
@@ -201,8 +208,5 @@ for file in test_files:
 
 chunked_test = chunk_python_scripts(content_to_chunk)
 
-# Print the chunks
-for index, text in enumerate(chunked_test):
-    output_fname = test_outputs + "/chunk" + str(index) + ".txt"
-    with open(output_fname, "w", encoding='utf-8') as f:
-        f.write(text)
+# Write the chunks to files
+write_chunks_to_files(chunked_test, test_outputs)
